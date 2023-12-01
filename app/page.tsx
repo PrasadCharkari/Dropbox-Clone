4b64735772791a96fc4417618e7d2a2b208c60cb
PrasadCharkari <141020733+PrasadCharkari@users.noreply.github.com>
2023-12-01 21:08:38 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>Drop Box</h1>
-      <h1>Drop Box</h1>
+      <UserButton />
     </main>
   )
 }
