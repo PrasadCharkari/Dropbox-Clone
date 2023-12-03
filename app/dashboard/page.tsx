6e4a62696fa6522a2bae96731abfcc9bde8f72c0
PrasadCharkari <141020733+PrasadCharkari@users.noreply.github.com>
2023-12-03 22:58:45 +0530

    Implemented Skeleton Loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
 import { auth } from "@clerk/nextjs"
 import Dropzone from "@/components/Dropzone"
-function Dasboard() {
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
+import { FileType } from "@/typing";
+
+async function Dasboard() {
     const { userId } = auth();
+    const docsResults = await getDocs(collection(db, "users", userId!, "files"))
+    const skeletonFiles: FileType[] = docsResults.docs.map(doc => ({
+        id: doc.id,
+        filename: doc.data().filename || doc.id,
+        timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+        fullName: doc.data().fullName,
+        downloadURL: doc.data().downloadURL,
+        type: doc.data().type,
+        size: doc.data().size,
+    }))
     return (
         <div>
             <Dropzone />
@@ -11,3 +25,8 @@ function Dasboard() {
 
 
 export default Dasboard
+
+
+
+
+
